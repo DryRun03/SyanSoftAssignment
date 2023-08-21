a56773c8bef5004a5be6b83540e379a2f5b05a39
Pankaj Garg <pankaj.garg@oodles.io>
2023-08-21 17:16:01 +0530

    initial commit

---
+++ src/main/java/io/syansoft/SyanSoftAssignmentProjectApplication.java
@@ -0,0 +1,13 @@
+package io.syansoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyanSoftAssignmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyanSoftAssignmentProjectApplication.class, args);
+	}
+
+}
